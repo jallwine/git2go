c1c3464c94de27dbfd630434d8564d7ba82a6a21
John Allwine <jallwine86@gmail.com>
2017-03-23 20:56:32 +0000

    modified build to cross compile for windows using MinGW

--- script/build-libgit2-static.sh
+++ script/build-libgit2-static.sh
@@ -14,6 +14,12 @@ cmake -DTHREADSAFE=ON \
       -DCMAKE_C_FLAGS=-fPIC \
       -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
       -DCMAKE_INSTALL_PREFIX=../install \
+      -DCMAKE_SYSTEM_NAME=Windows \
+      -DCMAKE_INSTALL_PREFIX=/usr/x86_64-w64-mingw32 \
+      -DCMAKE_C_COMPILER=/usr/bin/x86_64-w64-mingw32-gcc \
+      -DCMAKE_CXX_COMPILER=/usr/bin/x86_64-w64-mingw32-g++ \
+      -DDLLTOOL=/usr/bin/x86_64-w64-mingw32-dlltool \
+      -DCGO_LDFLAGS="-lwinhttp -lcrypt32 -lrpcrt4 -lole32 -lmswsock -lwsock32" \
       .. &&
 
 cmake --build .
