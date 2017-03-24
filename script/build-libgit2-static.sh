29cb5f3917b39fe08b1d3a48fcd467d79d130519
John Allwine <jallwine86@gmail.com>
2017-03-24 16:56:45 +0000

    change Makefile and build script to cross compile to mac

--- script/build-libgit2-static.sh
+++ script/build-libgit2-static.sh
@@ -14,6 +14,12 @@ cmake -DTHREADSAFE=ON \
       -DCMAKE_C_FLAGS=-fPIC \
       -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
       -DCMAKE_INSTALL_PREFIX=../install \
+      -DCMAKE_SYSTEM_NAME=Darwin \
+      -DCMAKE_INSTALL_PREFIX=/usr/local \
+      -DCMAKE_C_COMPILER=/usr/local/osx-ndk-x86/bin/x86_64-apple-darwin15-clang \
+      -DCMAKE_CXX_COMPILER=/usr/local/osx-ndk-x86/bin/x86_64-apple-darwin15-clang++ \
+      -DCOREFOUNDATION_DIRS=/usr/local/osx-ndk-x86/SDK/MacOSX10.11.sdk/System/Library/Frameworks/CoreFoundation.framework \
+      -DSECURITY_DIRS=/usr/local/osx-ndk-x86/SDK/MacOSX10.11.sdk/System/Library/Frameworks/Security.framework \
       .. &&
 
 cmake --build .
